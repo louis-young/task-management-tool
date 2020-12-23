a5520c29954727972ba6a9b889a682c23f863a6b
Louis Young <me@louisyoung.co.uk>
2020-12-23 12:59:48 +0000

    Refactored no `tasks` error handling

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,18 +7,16 @@ import Task from "../Task/Task";
 import "./List.scss";
 
 const List = ({ tasks, dispatch }) => {
-  if (!tasks) {
-    return null;
-  }
-
   return (
-    <section>
-      <ul className="list">
-        {tasks.map((task) => (
-          <Task task={task} dispatch={dispatch} key={task.id} />
-        ))}
-      </ul>
-    </section>
+    tasks && (
+      <section>
+        <ul className="list">
+          {tasks.map((task) => (
+            <Task task={task} dispatch={dispatch} key={task.id} />
+          ))}
+        </ul>
+      </section>
+    )
   );
 };
 
